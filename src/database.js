7e7582228d8283363e03f0e332c1e261624f9f69
edinson_figueroa <figueroaedinson@gmail.com>
2021-07-18 12:55:10 -0500

    var de entorno congig

--- src/database.js
+++ src/database.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
-const DB = 'mongodb://localhost/mern-task';
+const DB = process.env.MONGODB_URI;
 
 mongoose.connect(DB)
     .then(db => console.log('DB is connected'))
     .catch(err => console.log(err));
 
     module.exports = mongoose;
+
--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ const { join } = require('path');
 const path = require('path');
 const app = express();
 const route = require('./routes/task.routes');
+const dotenv = require('dotenv').config();
 const  mongoose  = require('./database.js');
 const cors = require('cors');
+
 //Setting
  app.set('port', process.env.PORT || 3000);
  const PORT = app.get('port');
